8d082f66de571dfeadfd6b62da51b674b5b7f830
Sudhanshu G Gaikwad <140872777+sggaikwad@users.noreply.github.com>
2025-06-30 21:51:49 +0530

    your message

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import Provider from "./Provider";
+
+import Providers from "./Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <Provider>{children}</Provider>
+          <Providers>{children}</Providers>
         </body>
       </html>
     </ClerkProvider>
